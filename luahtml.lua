c06f688cf5f071ad6e044dd574ad734e22cc5f1f
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2021-07-26 02:35:17 +0300

    Create luahtml.lua

---
+++ luahtml.lua
@@ -0,0 +1,63 @@
+local lines = {}
+local sbox = setmetatable({}, { __index = _G })
+
+local function printLine(...)
+    local ret = {}
+    for i = 1, select("#", ...) do
+        local arg = tostring(select(i, ...))
+        table.insert(ret, arg)
+    end
+    return table.concat(ret, "\t")
+end
+
+sbox.PrintTable = function(t)
+    table.insert(lines, table.ToPlain(t))
+end
+
+sbox.print = function(...)
+    table.insert(lines, printLine(...))
+end
+
+function EvalLuaHTML(luahtml)
+    lines = {}
+    local error = false
+
+    local output = string.gsub(luahtml, "<lua>(.-)</lua>", function(code)
+        local fn, syntaxError = load(code, "<lua>...</lua>", "t", sbox)
+        if not fn then
+            error = (error and error .."\n" or "") .. syntaxError
+            return ""
+        end
+
+        local success, runtimeError = pcall(fn)
+        if not success then
+            error = (error and error .."\n" or "") .. runtimeError
+            return ""
+        end
+
+        return ""
+    end)
+
+    return error or output
+end
+
+--[[
+print(
+    EvalLuaHTML("<h1><lua> print('Test') </lua></h1>")
+)
+]]--
+
+function EvalLuaHTMLFile(path)
+    local f = io.open(path, "r")
+    if not f then return false, "" end
+    local luahtml = f:read("*a")
+    io.close(f)
+
+    return true, EvalLuaHTML(luahtml)
+end
+
+--[[
+print(
+    EvalLuaHTMLFile("var/www/site.com/pages/index.lua.html")
+)
+]]--
